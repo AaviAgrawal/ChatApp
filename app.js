538f82c2e4744afee761e4332a379160362d604d
AaviAgrawal <aaviagrawal212@gmail.com>
2025-04-30 17:24:52 +0530

    added typing feature

--- app.js
+++ app.js
@@ -1,48 +1,59 @@
 const express = require("express");
 const app = express();
 
-const soketIO = require("socket.io");
+const socketIO = require("socket.io");
 const http = require("http");
 const server = http.createServer(app);
-const io = soketIO(server);
+const io = socketIO(server);
 const unames = [];
 const userids = [];
 
-io.on("connection",function(socket){
-    socket.on("message",function(message){
-        let index = userids.indexOf(socket.id);
-        let name = unames[index];
-        io.emit("message",{message,name,id:socket.id});
-    })
-    socket.on("typing",function(){
-        let index = userids.indexOf(socket.id);
-        let name = unames[index];
+io.on("connection", function (socket) {
+    console.log("Socket.io connection established");
+    console.log("User connected with id: ", socket.id);
 
-        socket.broadcast.emit("typing",{name});
-    })
-    socket.on("uname",function(name){
+    socket.on("message", function (message) {
+        let index = userids.indexOf(socket.id);
+        if (index !== -1) {
+            let name = unames[index];
+            io.emit("message", { message, name, id: socket.id });
+            console.log("User sent message: ", message);
+        }
+    });
+
+    socket.on("typing", function () {
+        let index = userids.indexOf(socket.id);
+        if (index !== -1) {
+            let name = unames[index];
+            socket.broadcast.emit("typing", { name });
+            console.log("User is typing...");
+        }
+    });
+
+    socket.on("uname", function (name) {
         unames.push(name);
         userids.push(socket.id);
         socket.emit("setnamedone");
-        io.emit("countofpeople",unames);console.log("Server started on port 3000");
-console.log("Socket.io connection established");
-console.log("User connected with id: ", socket.id);
-console.log("User disconnected with id: ", socket.id);
-console.log("User sent message: ", message);
-console.log("User is typing...");
-console.log("User set name: ", name);
-console.log("User count updated: ", unames.length);
-    })
-    socket.on("disconnect",function(){
+        io.emit("countofpeople", unames);
+        console.log("User set name: ", name);
+        console.log("User count updated: ", unames.length);
+    });
+
+    socket.on("disconnect", function () {
         let index = userids.indexOf(socket.id);
-        unames.splice(index,1);
-        io.emit("countofpeople",unames.length);
-    })
-})
+        if (index !== -1) {
+            unames.splice(index, 1);
+            userids.splice(index, 1);
+            io.emit("countofpeople", unames);
+            console.log("User disconnected with id: ", socket.id);
+            console.log("User count updated: ", unames.length);
+        }
+    });
+});
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.render("index");
 });
 
